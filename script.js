60a25b235a500bab3fd604aab10a48dca6e00a6d
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-04-01 18:27:00 -0300

    css

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function select(){
+
+}
+
+
+// mudar botão de confirmar:
+//if(food, drink e desert selecionado){
+    const confirmBttn = document.querySelector('.confirm'); //pega o botao
+    const confirmTxt = document.querySelector('.confirm-txt'); //pega o texto do butão
+
+    confirmTxt.innerHTML = 'Fechar Pedido';
+    //confirmBttn.innerHTML = fica verde
+//}
+
+//if(botão verde apertado){
+// aparecer a janelinha de confirmar
+// falta criar a caixinha verde 
+// const caixinha verde = document.querySelector(.caixinha verde)
+// painel.classlist.remove('escondido'); 
+//}
