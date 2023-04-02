63aacbcff014574ba4e85bb13e67406f8cf6e52f
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-04-02 20:16:16 -0300

    adicionado js
    Adicionado JS para:
    - Mostrar a opção selecionada
    - Mudar o botão após escolher os 3 itens

--- script.js
+++ script.js
@@ -1,21 +1,70 @@
+/* 
+let nameFood = '';
+let nameDrink = '';
+let nameDesert = ''; 
 
-function select(){
+function verify(){
+    if(nameFood !== ''){
+        if(nameDrink !== ''){
+            if(nameDesert !== ''){
+                const buttonContinue = document.querySelector('.confirm');
+                buttonContinue.classList.add('green-button');
+            }
+        }
+    }
 
 }
 
+function changeButton(bttn){
+    //if(food, drink e desert selecionado){
+        const confirmTxt = document.querySelector('.confirm-txt'); //pega o texto do butão
+        confirmTxt.innerHTML = 'Fechar Pedido';
+        bttn.classList.add('green-button');
 
-// mudar botão de confirmar:
-//if(food, drink e desert selecionado){
-    const confirmBttn = document.querySelector('.confirm'); //pega o botao
-    const confirmTxt = document.querySelector('.confirm-txt'); //pega o texto do butão
+    //}
+    verify();
+} */
 
-    confirmTxt.innerHTML = 'Fechar Pedido';
-    //confirmBttn.innerHTML = fica verde
-//}
+let check1;
+let check2;
+let check3;
 
-//if(botão verde apertado){
-// aparecer a janelinha de confirmar
-// falta criar a caixinha verde 
-// const caixinha verde = document.querySelector(.caixinha verde)
-// painel.classlist.remove('escondido'); 
-//}
+function selectFood(opt1){
+    const selectedFood = document.querySelector('.food .green-border ');
+    if(selectedFood !== null){
+        selectedFood.classList.remove('green-border');
+    }
+    opt1.classList.add('green-border');
+    
+   check1 = 1;
+    orderComplete();
+}
+function selectDrink(opt2){
+    const selectedDrink = document.querySelector('.drink .green-border ');
+    if(selectedDrink !== null){
+        selectedDrink.classList.remove('green-border');
+    }
+    opt2.classList.add('green-border');
+
+    check2 = 2;
+    orderComplete();
+}
+function selectDesert(opt3){
+    const selectedDesert = document.querySelector('.desert .green-border ');
+    if(selectedDesert !== null){
+        selectedDesert.classList.remove('green-border');
+    }
+    opt3.classList.add('green-border');
+    check3 = 3;
+    orderComplete();
+}
+
+function orderComplete() {
+
+    if(check1 === 1 && check2 === 2 && check3 === 3){
+        document.getElementById("green-button").style.display = "flex";
+        document.getElementById("grey-button").style.display = "none";
+
+    }
+
+}
